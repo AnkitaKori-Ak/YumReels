b272e17885bfd68913f6ac3bd2c3aac2bf75d44a
AnkitaKori-Ak <koriankita.01@gmail.com>
2025-12-08 17:13:52 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -28,7 +28,13 @@ async function registerUser(req,res) {
     const token = jwt.sign({
         id:user._id,
     },process.env.JWT_SECRET)
-    res.cookie("token",token)
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,        
+    sameSite: "none",    
+    path: "/"
+});
+
 
     res.status(201).json({
         message:"User Registered successfully",
@@ -63,7 +69,13 @@ async function loginUser(req,res){
         id:user._id,
     },process.env.JWT_SECRET)
 
-    res.cookie("token",token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,       
+    sameSite: "none",    
+    path: "/"
+});
+
     res.status(200).json({
         message:"User logged in successfully",
          user:{
